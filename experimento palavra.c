82d1f0d7cf47d02b79ae223551aa3840ceb8d6c9
Renan Coelho <50117002+renancoelhodev@users.noreply.github.com>
2019-06-16 18:19:26 -0300

    update com passa a vez

--- experimento palavra.c
+++ experimento palavra.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <locale.h>
+#include <conio.h>
+
 
 char palavra[10];
 char palavratotal[10];
 int i;
 char letra;
-int acertos;
 float premios[] = {10, 20, 30};
 float acumulado = 0;
 int premioale;
-int acertou = 0;
+int acertos = 0;
 
 main(){
 	
 	printf("Digite uma palavra: ");
-	scanf("%s", &palavra);
+	gets(palavra);
 	fflush(stdin);
+	strupr(palavra);
+	
 	
 	srand(time(NULL));
 	
@@ -26,42 +30,42 @@ main(){
 	}
 
 do{
+	setlocale(LC_ALL, "Portuguese");
 	premioale = rand() % 100;
-	printf(" Valendo R$%d.00 por letra.", premioale);
+	printf("\nValendo R$%d.00 por letra.", premioale);
 	printf("\nDigite uma letra: ");
 	scanf("%c", &letra);
+	
+	letra = toupper(letra);
+	
 	fflush(stdin);
+	
+	acertos = 0;
 	
 	for (i=0; palavra[i] != '\0'; i++){
 		
-			if (palavra[i] == letra){
-				printf(" %c", letra);
-				palavratotal[i] = letra;
-				acertos = acertos + 1;
-				acumulado = acumulado + premioale;
-				acertou = 1;
-				
-				
+		if (palavra[i] == letra){
+			printf(" %c", letra);
+			palavratotal[i] = letra;
+			acumulado = acumulado + premioale;
+			acertos++;
+		}
+			else if (palavratotal[i] == palavra[i]){
+				printf(" %c", palavratotal[i]);
 			}
-				else if (palavratotal[i] == palavra[i]){
-					printf(" %c", palavratotal[i]);
-					acertou = 1;
-					
+				else{
+					printf(" _");
 				}
-					else if (acertou == 1){
-						acertou = 1;
-						printf(" _");
-					}
-						else{
-							acertou = 0;
-							exit (0);
-						}
 	}
-
-}
-	while (strcmp (palavra, palavratotal) == 1 && acertou == 1);
-	printf("\nAcertos: %d", acertos);
-	printf("\nValor total em premios: %.2f", acumulado);
-	exit (0);
 	
+		if ( acertos>0){  
+           printf ("\n\nPrêmio acumulado = R$ %.2f", acumulado);
+        }
+        	else{
+          	 printf ("\n\nVocê PERDEU A VEZ, pois a letra informa '%c' NÃO faz parte da palavra", letra);
+          	}
+    getch();
+    
+}
+	while (strcmp (palavra, palavratotal) && acertos!=0 );
 }
