ddf4cacaeafed7a4cd2688e109a78b4ebdd421d3
Renan Coelho <50117002+renancoelhodev@users.noreply.github.com>
2019-06-16 14:58:22 -0300

    com premio acumulado

--- experimento palavra.c
+++ experimento palavra.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 char palavra[10];
 char palavratotal[10];
 int i;
 char letra;
+int acertos;
+float premios[] = {10, 20, 30};
+float acumulado = 0;
+int premioale;
 
 main(){
 	
@@ -13,12 +18,15 @@ main(){
 	scanf("%s", &palavra);
 	fflush(stdin);
 	
+	srand(time(NULL));
+	
 	for (i=0; palavra[i] != '\0'; i++){
 		printf("_ ");
 	}
 
 do{
-
+	premioale = rand() % 100;
+	printf(" Valendo R$%d.00 .", premioale);
 	printf("\nDigite uma letra: ");
 	scanf("%c", &letra);
 	fflush(stdin);
@@ -28,6 +36,8 @@ do{
 			if (palavra[i] == letra){
 				printf(" %c", letra);
 				palavratotal[i] = letra;
+				acertos = acertos + 1;
+				acumulado = acumulado + premioale;
 				
 			}
 				else if (palavratotal[i] == palavra[i]){
@@ -40,5 +50,8 @@ do{
 
 }
 	while (strcmp (palavra, palavratotal) == 1 );
-	exit (0);	
+	printf("\nAcertos: %d", acertos);
+	printf("\nValor total em premios: %.2f", acumulado);
+	exit (0);
+	
 }
